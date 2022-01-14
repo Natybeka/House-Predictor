be89080d5817b7dca352f0809e6247ed3f4d44e7
Natnael Bekabtu <se.natnael.bekabtu@gmail.com>
2022-01-14 17:48:57 +0300

    user registration added

--- ML model/app.py
+++ ML model/app.py
@@ -1,18 +1,38 @@
-from flask import Flask
+from flask import Flask, request
 from train import trainMachine
+from sklearn.datasets import make_blobs
 import os
+import pandas as pd
+import numpy as np
+import joblib
 app = Flask(__name__)
 
 # training the model for the first time
-if not os.path.isfile('./models/models.pki'):
+if os.path.isfile('./models/models.pki') == False:
     trainMachine()
 
 
-@app.route("/predict", methods=['GET'])
+@app.route("/predict", methods=['POST'])
 def predict():
+    jsonData = request.json
 
-    checkForUpdatedData()
-    return "<p>Hello, World!</p>"
+    features = pd.array(['LotArea', 'YearBuilt', '1stFlrSF',
+                         '2ndFlrSF', 'FullBath', 'BedroomAbvGr', 'TotRmsAbvGrd'])
+    iowa_model = joblib.load("./models/model.pki")
+    data_input = [[jsonData["LotArea"],
+                  jsonData["YearBuilt"], jsonData["1stFlrSF"],
+                  jsonData["2ndFlrSF"], jsonData["FullBath"],
+                  jsonData["BedroomAbvGr"], jsonData["TotRmsAbvGrd"]]]
+
+    data_input = pd.array(data_input, dtype=np.dtype("int32"))
+
+    print(data_input)
+
+    predicted_sale_price = iowa_model.predict(data_input)
+
+    print(predicted_sale_price)
+    # checkForUpdatedData()
+    return "predicted_sale_price", 200
 
 
 # helper function for checking the updated data
--- ML model/train.py
+++ ML model/train.py
@@ -3,6 +3,8 @@ from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeRegressor
 import joblib
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error
 
 
 def trainMachine():
@@ -16,16 +18,17 @@ def trainMachine():
     features = ['LotArea', 'YearBuilt', '1stFlrSF',
                 '2ndFlrSF', 'FullBath', 'BedroomAbvGr', 'TotRmsAbvGrd']
     X = home_data[features]
+    print(X.head())
 
     # Split into validation and training data
     # This prevents overfitting and makes the model's predictions accurate
     train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=1)
 
     # Specify Model
-    iowa_model = DecisionTreeRegressor(random_state=1)
+    iowa_model = RandomForestRegressor(random_state=1)
     # Fit Model
     iowa_model.fit(train_X, train_y)
-
+    print(val_X.head())
     # Make validation predictions and calculate mean absolute error
     val_predictions = iowa_model.predict(val_X)
     val_mae = mean_absolute_error(val_predictions, val_y)
